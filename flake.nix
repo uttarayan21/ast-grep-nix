205f45df9299105e7b896bc6d7dedce3776217fd
uttarayan21 <email@uttarayan.me>
2024-10-24 17:50:27 +0530

    feat: Added more options for ast-grep / sg

--- flake.nix
+++ flake.nix
@@ -3,11 +3,24 @@
   inputs = {};
   outputs = {self}: {
     mkLib = pkgs: rec {
-      scanWithOptions = src: {config ? "${src}/sgconfig.toml"}:
+      scanWithOptions = src: {
+        config ? "${src}/sgconfig.yml",
+        color ? "ansi",
+        format ? null,
+        # json ? false,
+      }: let
+        vColor = pkgs.lib.asserts.assertOneOf "color" color ["auto" "always" "never" "ansi"];
+        vFormat = pkgs.lib.asserts.assertOneOf "format" format ["github" null];
+        # vJson = pkgs.lib.asserts.assertOneOf "json" json [true false "pretty" "stream" "compact"];
+      in
         pkgs.runCommand "ast-grep.scan" {
+          inherit src;
           nativeBuildInputs = [pkgs.ast-grep];
         } ''
-          ast-grep scan -c ${config}
+          sg scan -c ${config} ${src} \
+            ${pkgs.lib.optionalString vColor "--color ${color}"} \
+            ${pkgs.lib.optionalString (vFormat && !(isNull format)) "--format ${format}"} \
+            > $out
         '';
       scan = src: scanWithOptions src {};
     };
